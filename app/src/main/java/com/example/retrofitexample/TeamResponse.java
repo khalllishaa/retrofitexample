863e41e5163bfde206fbb740cfc7a8c8537db3a4
khalllishaa <kkaylanasywa@gmail.com>
2024-05-14 09:42:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/retrofitexample/TeamResponse.java
@@ -0,0 +1,13 @@
+package com.example.retrofitexample;
+
+
+import java.util.List;
+
+public class TeamResponse {
+    private List<Team> teams;
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+}
+// yang menampilakan hasil team, menggunakanrv
